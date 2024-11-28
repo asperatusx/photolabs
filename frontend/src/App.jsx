02ea00bfaf286fd5f34a1892a39e9292d402d138
Andrew <andrewlang.p@gmail.com>
2024-11-28 18:58:27 -0500

    use homeRoute to display top navigation bar and photolist

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-
-import PhotoList from 'components/PhotoList';
 import './App.scss';
-import TopicList from 'components/TopicList';
-import TopNavigationBar from 'components/TopNavigationBar';
+
+import HomeRoute from 'routes/HomeRoute';
 
 
 // Note: Rendering a single component to build components in isolation
@@ -15,9 +13,7 @@ const App = () => {
   
   return (
     <div className="App">
-      <TopNavigationBar />
-      {/* <TopicList />
-      <PhotoList /> */}
+      <HomeRoute />
     </div>
   );
 };
