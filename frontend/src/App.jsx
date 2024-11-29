3805a737f3f544ed9c3bc8424db19c0241523952
Andrew <andrewlang.p@gmail.com>
2024-11-29 18:58:16 -0500

    add close button to modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
   return (
     <div className="App">
       <HomeRoute photosData={photos} topicsData={topics} toggleFavPhoto={toggleFavPhoto} favPhotos={favPhotos} toggleModal={toggleModal}/>
-      { modal && <PhotoDetailsModal /> }
+      { modal && <PhotoDetailsModal toggleModal={toggleModal}/> }
     </div>
   );
 };
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+  const { toggleModal } = props;
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button onClick={toggleModal} className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
