612b9c375e4bc744fdf254820b5320d04effcfe6
Andrew <andrewlang.p@gmail.com>
2024-12-01 04:03:36 -0500

    refactor the custom hook useApplicationData to use useReducer instead of useState

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,24 +1,58 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const ACTIONS = {
+  TOGGLE_MODAL: 'TOGGLE_MODAL',
+  HANDLE_FAV_BUTTON: 'HANDLE_FAV_BUTTON'
+}
+
+const initialState = {
+  favPhotos: [],
+  modal: false,
+  photoDetails: {}
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case ACTIONS.TOGGLE_MODAL:
+      return {
+        ...state,
+        modal: !state.modal,
+        photoDetails: action.payload || {}
+      }
+    case ACTIONS.HANDLE_FAV_BUTTON:
+      return {
+        ...state,
+        favPhotos: state.favPhotos.includes(action.payload) ? state.favPhotos.filter(id => id !== action.payload) : [...state.favPhotos, action.payload]
+      }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      )
+  }
+}
 
 const useApplicationData = () => {
-  const [favPhotos, setFavPhotos] = useState([]);
-  const [modal, setModal] = useState(false);
-  const [photoDetails, setPhotoDetails] = useState({});
+  // const [favPhotos, setFavPhotos] = useState([]);
+  // const [modal, setModal] = useState(false);
+  // const [photoDetails, setPhotoDetails] = useState({});
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const toggleModal = (e, photoInfo) => {
     e.stopPropagation();
-    setModal(prevModal => !prevModal)
-    setPhotoDetails(photoDetails => photoDetails = {...photoInfo})
+    // setModal(prevModal => !prevModal)
+    // setPhotoDetails({...photoInfo})
+    dispatch({type: ACTIONS.TOGGLE_MODAL, payload: photoInfo})
   }
 
   const handleFavButton = (photoId) => {
-    setFavPhotos(prevFav => {
-      return prevFav.includes(photoId) ? prevFav.filter(id => id !== photoId) : [...prevFav, photoId]
-    })
+    // setFavPhotos(prevFav => {
+    //   return prevFav.includes(photoId) ? prevFav.filter(id => id !== photoId) : [...prevFav, photoId]
+    // })
+    dispatch({type: ACTIONS.HANDLE_FAV_BUTTON, payload: photoId})
   }
 
   return {
-    favPhotos, modal, photoDetails, toggleModal, handleFavButton
+    favPhotos: state.favPhotos, modal: state.modal, photoDetails: state.photoDetails, toggleModal, handleFavButton
   }
 }
 
